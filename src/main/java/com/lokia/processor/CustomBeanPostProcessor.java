1a1a12880d23159901e5e451bb9e22ee95f9b5b2
wb-gs313456 <wb-gs313456@alibaba-inc.com>
2018-07-20 11:34:25 +0800

    the bean create and initialize process is: beforeInitialization -> afterPropertiesSet -> afterInitialization; the first and third is provided by BeanPostProcessor

---
+++ src/main/java/com/lokia/processor/CustomBeanPostProcessor.java
@@ -0,0 +1,24 @@
+package com.lokia.processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author gushu
+ * @data 2018/7/20
+ */
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor{
+
+    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+        System.out.println("before initialization bean:"+beanName);
+        return bean; // we could potentially return any object reference here...
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("after initialization bean:"+beanName);
+        return bean;
+    }
+
+}
