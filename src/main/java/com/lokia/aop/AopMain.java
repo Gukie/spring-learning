a9b325608ea99247c2a756d027f084bef708c007
wb-gs313456 <wb-gs313456@alibaba-inc.com>
2018-07-23 15:21:06 +0800

    JDK dynamic proxy只能代理 interface. 如果代理的类没有实现interface，会报错. 因为底层是通过 Proxy.newProxyInstance实现的

--- src/main/java/com/lokia/aop/AopMain.java
+++ src/main/java/com/lokia/aop/AopMain.java
@@ -1,6 +1,10 @@
 package com.lokia.aop;
 
 import com.lokia.aop.cglib.CglibProxyServiceImpl;
+import com.lokia.aop.custom.CustomInvocationHandler;
+import com.lokia.aop.custom.CustomInvocationService;
+import com.lokia.aop.custom.CustomInvocationServiceImpl;
+import com.lokia.aop.custom.CustomProxyFactory;
 import com.lokia.aop.jdk.JdkDynamicProxyService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -23,5 +27,12 @@ public class AopMain {
         System.out.println();
         JdkDynamicProxyService jdkDynamicProxyService = applicationContext.getBean(JdkDynamicProxyService.class);
         jdkDynamicProxyService.test();
+
+        // custom invocation handler
+        System.out.println();
+        CustomInvocationService customInvocationService = applicationContext.getBean(CustomInvocationService.class);
+        CustomInvocationService proxiedOne = (CustomInvocationService) CustomProxyFactory.createProxyObj(customInvocationService);
+        proxiedOne.test();
+
     }
 }
---
+++ src/main/java/com/lokia/aop/custom/CustomInvocationService.java
@@ -0,0 +1,10 @@
+package com.lokia.aop.custom;
+
+/**
+ * @author gushu
+ * @data 2018/7/23
+ */
+public interface CustomInvocationService {
+
+    void test();
+}
