b878de4b7ac21683081e1e978a492922468697c4
wb-gs313456 <wb-gs313456@alibaba-inc.com>
2018-07-24 12:00:14 +0800

    only contextRefreshedEvent will be triggered. other event should be triggered manually

--- src/main/java/com/lokia/event/BlackEventMain.java
+++ src/main/java/com/lokia/event/BlackEventMain.java
@@ -2,6 +2,8 @@ package com.lokia.event;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 /**
  * @author gushu
@@ -11,10 +13,22 @@ public class BlackEventMain {
 
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.scan("com.lokia.event");
-        context.refresh();
+//        // manually trigger
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+//        context.scan("com.lokia.event");
+//        context.refresh();
+//
+//        BlackListEventPublisher eventPublisher = context.getBean(BlackListEventPublisher.class);
+//        for (int i = 0; i < 3; i++) {
+//            eventPublisher.publishBlackItem("" + i);
+//        }
+//        context.stop();
+//        context.start();
+//        context.close();
 
+
+        // only contextRefreshedEvent will be triggered. other event should be triggered manually
+        ApplicationContext context = new GenericXmlApplicationContext("spring.xml");
         BlackListEventPublisher eventPublisher = context.getBean(BlackListEventPublisher.class);
         for (int i = 0; i < 3; i++) {
             eventPublisher.publishBlackItem("" + i);
